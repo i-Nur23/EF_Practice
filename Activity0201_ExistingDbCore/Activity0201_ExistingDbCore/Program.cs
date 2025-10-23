0ff39eb19423205515643df2d2877c7bdb331c64
Ainur Iskhakov <aiinuur2314@gmail.com>
2025-10-23 21:24:33 +0300

    Initial commit

---
+++ Activity0201_ExistingDbCore/Activity0201_ExistingDbCore/Program.cs
@@ -0,0 +1,46 @@
+﻿using EF_Activity001;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+public class Program
+{
+    static IConfigurationRoot _configuration;
+    static DbContextOptionsBuilder<AdventureWorks2019Context> _optionsBuilder;
+
+    private static void Main(string[] args)
+    {
+        BuildConfiguration();
+        BuildOptions();
+        ListPeople();
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+
+    static void BuildOptions()
+    {
+        _optionsBuilder = new DbContextOptionsBuilder<AdventureWorks2019Context>();
+        _optionsBuilder.UseSqlServer(_configuration.GetConnectionString("AdventureWorks"));
+    }
+
+    static void BuildConfiguration()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+        _configuration = builder.Build();
+    }
+
+    static void ListPeople()
+    {
+        using (var db = new AdventureWorks2019Context(_optionsBuilder.Options))
+        {
+            var people = db.People.OrderByDescending(x => x.LastName).Take(20).
+            ToList();
+            foreach (var person in people)
+            {
+                Console.WriteLine($"{person.FirstName} {person.LastName}");
+            }
+        }
+    }
+}
---
+++ EF_Activity001/EF_Activity001/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EF_Activity001
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
