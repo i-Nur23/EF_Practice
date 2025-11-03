998925080aaca211aef0a68cc333e483217f417c
Ainur Iskhakov <aiinuur2314@gmail.com>
2025-11-04 00:05:14 +0300

    Finished chapter 3 - code-first approach

---
+++ Activity0302_EFCoreNewDb/InventoryDatabaseCore/InventoryDbContext.cs
@@ -0,0 +1,30 @@
+﻿using InventoryModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace InventoryDatabaseCore
+{
+    public class InventoryDbContext : DbContext
+    {
+        static IConfigurationRoot _configuration;
+
+        public DbSet<Item> Items { get; set; }
+
+        public InventoryDbContext() : base() { }
+
+        public InventoryDbContext(DbContextOptions options) : base(options) 
+        { 
+        
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                         .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            _configuration = builder.Build();
+            var cnstr = _configuration.GetConnectionString("InventoryManager");
+            optionsBuilder.UseSqlServer(cnstr);
+        }
+    }
+}
