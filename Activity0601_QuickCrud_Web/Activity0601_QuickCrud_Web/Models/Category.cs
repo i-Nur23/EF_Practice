a6de847e8ac11e6dd5abbb14427d1ad54caf8c5f
Ainur Iskhakov <aiinuur2314@gmail.com>
2025-11-08 00:51:54 +0300

    Made activity 0601

---
+++ Activity0601_QuickCrud_Web/Activity0601_QuickCrud_Web/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Activity0601_QuickCrud_Web.Models
+{
+    public class Category
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string? Name { get; set; }
+
+        public virtual List<Item> Items { get; set; } = new List<Item>();
+    }
+}
---
+++ Activity0601_QuickCrud_Web/Activity0601_QuickCrud_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Activity0601_QuickCrud_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
